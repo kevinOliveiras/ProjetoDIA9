a51fe8d53350facb1fe00c02601292d25f042fb8
Igor <igorcordeiro@id.uffbr>
2023-10-04 21:48:16 -0300

    Criando Menu

--- ProjetoDIA9/Program.cs
+++ ProjetoDIA9/Program.cs
@@ -1,15 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProjetoDIA9
+﻿namespace ProjetoDIA9
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Empresa empresa = new Empresa();
+
+            while (true)
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Cadastrar Empregado:");
+                Console.WriteLine("2. Listar todos Empregados:");
+                Console.WriteLine("3. Promover um Empregado:");
+                Console.WriteLine("4. Demitir um Empregado:");
+                Console.WriteLine("5. Listar salario anual do Empregado:");
+                Console.WriteLine("6. Sair");
+                Console.WriteLine("Escolhe uma opçao:");
+
+                int op = int.Parse(Console.ReadLine());
+                    
+                switch (op)
+                {
+                    case 1:
+
+                    case 2:
+
+
+                    case 3:
+
+
+                    case 4:
+
+
+                    case 5:
+
+
+                    case 6:
+                        break;
+                }
+            }
 
         }
     }
